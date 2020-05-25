44cce33cfa93ec70c5caf7c35f5fbb62e4dec9b8
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-25 13:32:18 -1000

    added TV Maze API library

---
+++ tvmazeapi.py
@@ -0,0 +1,55 @@
+#v.0.1.0
+
+from resources.lib.url import URL
+JSONURL = URL( 'json' )
+TXTURL = URL()
+
+
+class TVMaze( object ):
+
+    def __init__( self, user='', apikey='' ):
+        self.PUBLICURL = 'https://api.tvmaze.com'
+        if user and apikey:
+            self.AUTHURL = 'https://%s:%s@api.tvmaze.com/v1/user' % (user, apikey)
+        else:
+            self.AUTHURL = self.PUBLICURL
+
+
+    def getShow( self, tvmazeid, params=None ):
+        return self._call( 'shows/%s' % tvmazeid, params )
+
+
+    def getEpisode( self, episodeid, params=None ):
+        return self._call( 'episodes/%s' % episodeid, params )
+
+
+    def getFollowedShows( self, params=None ):
+        return self._call( 'follows/shows', params, auth=True )
+
+
+    def getTaggedShows( self, tag, params=None ):
+        return self._call( 'tags/%s/shows' % tag, params, auth=True )
+
+
+    def unTagShow( self, show, tag, params=None ):
+        return self._call( 'tags/%s/shows/%s' % (tag, show), params, auth=True, type='delete' )
+
+
+    def _call( self, url_end, params, auth=False, type="get" ):
+        loglines = []
+        if not params:
+            params = {}
+        if auth:
+            if self.AUTHURL == self.PUBLICURL:
+                loglines.append( 'authorization credentials required but not supplied' )
+                return False, loglines, {}
+            url_base = self.AUTHURL
+        else:
+            url_base = self.PUBLICURL
+        url = '%s/%s' % (url_base, url_end )
+        if type == 'get':
+            success, j_loglines, results = JSONURL.Get( url, params=params )
+        if type == 'delete':
+            success, j_loglines, results = TXTURL.Delete( url, params=params )
+        loglines = loglines + j_loglines
+        return success, loglines, results
