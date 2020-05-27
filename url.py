8819886eb56b1ac05f97c4279618fa1488b83df1
Kyle Johnson <kyle.johnson@alumni.duke.edu>
2020-05-27 11:04:21 -1000

    better handling of 4xx and 5xx codes and return of status codes

--- url.py
+++ url.py
@@ -1,101 +1,107 @@
-#v.0.4.1
+#v.0.5.0
 
 import socket
 import requests as _requests
 
-
 class URL( object ):
 
     def __init__( self, returntype='text', headers='', timeout=10 ):
-        self.timeout = timeout
-        self.headers = headers
-        self.returntype = returntype
+        self.TIMEOUT = timeout
+        self.HEADERS = headers
+        self.RETURNTYPE = returntype
 
 
-    def Get( self, url, **kwargs ):
-        params, data = self._unpack_args( kwargs )
-        return self._urlcall( url, params, '', 'get' )
+    def Get( self, theurl, **kwargs ):
+        return self._urlcall( theurl, 'get', kwargs )
 
 
-    def Post( self, url, **kwargs ):
-        params, data = self._unpack_args( kwargs )
-        return self._urlcall( url, params, data, 'post' )
+    def Post( self, theurl, **kwargs ):
+        return self._urlcall( theurl, 'post', kwargs )
 
 
-    def Put( self, url, **kwargs ):
-        params, data = self._unpack_args( kwargs )
-        return self._urlcall( url, params, data, 'put' )
+    def Put( self, theurl, **kwargs ):
+        return self._urlcall( theurl, 'put', kwargs )
 
 
-    def Delete( self, url, **kwargs ):
-        params, data = self._unpack_args( kwargs )
-        return self._urlcall( url, params, data, 'delete' )
+    def Delete( self, theurl, **kwargs ):
+        return self._urlcall( theurl, 'delete', kwargs )
 
 
-    def _urlcall( self, url, params, data, urltype ):
+    def _urlcall( self, theurl, urltype, kwargs ):
         loglines = []
         urldata = ''
+        bad_r = False
+        auth, params, thedata = self._unpack_args( kwargs )
         try:
             if urltype == "get":
-                urldata = _requests.get( url, params=params, headers=self.headers, timeout=self.timeout )
+                urldata = _requests.get( theurl, params=params, headers=self.HEADERS, auth=auth, timeout=self.TIMEOUT )
             elif urltype == "post":
-                urldata = _requests.post( url, params=params, data=data, headers=self.headers, timeout=self.timeout )
+                urldata = _requests.post( theurl, params=params, data=thedata, headers=self.HEADERS, timeout=self.TIMEOUT )
             elif urltype == "put":
-                urldata = _requests.put( url, params=params, data=data, headers=self.headers, timeout=self.timeout )
+                urldata = _requests.put( theurl, params=params, data=thedata, headers=self.HEADERS, timeout=self.TIMEOUT )
             elif urltype == "delete":
-                urldata = _requests.delete( url, params=params, data=data, headers=self.headers, timeout=self.timeout )
+                urldata = _requests.delete( theurl, params=params, data=thedata, headers=self.HEADERS, timeout=self.TIMEOUT )
             loglines.append( "the url is: " + urldata.url )
             loglines.append( 'the params are: ')
             loglines.append( params )
             loglines.append( 'the data are: ')
-            loglines.append( data )
+            loglines.append( thedata )
+            urldata.raise_for_status()
         except _requests.exceptions.ConnectionError as e:
-            loglines.append( 'site unreachable at ' + url )
+            loglines.append( 'site unreachable at ' + theurl )
             loglines.append( e )
+            bad_r = True
         except _requests.exceptions.Timeout as e:
-            loglines.append( 'timeout error while downloading from ' + url )
+            loglines.append( 'timeout error while downloading from ' + theurl )
             loglines.append( e )
+            bad_r = True
         except socket.timeout as e:
-            loglines.append( 'timeout error while downloading from ' + url )
+            loglines.append( 'timeout error while downloading from ' + theurl )
             loglines.append( e )
+            bad_r = True
         except _requests.exceptions.HTTPError as e:
-            loglines.append( 'HTTP Error while downloading from ' + url )
+            loglines.append( 'HTTP Error while downloading from ' + theurl )
             loglines.append( e )
+            bad_r = True
         except _requests.exceptions.RequestException as e:
-            loglines.append( 'unknown error while downloading from ' + url )
+            loglines.append( 'unknown error while downloading from ' + theurl )
             loglines.append( e )
+            bad_r = True
+        if bad_r:
+            return False, loglines, ''
         if urldata:
             success = True
-            loglines.append( 'returning URL as ' + self.returntype )
-            if self.returntype == 'text':
+            loglines.append( 'returning URL as ' + self.RETURNTYPE )
+            if self.RETURNTYPE == 'text':
                 data = urldata.text
-            elif self.returntype == 'binary':
+            elif self.RETURNTYPE == 'binary':
                 data = urldata.content
-            elif self.returntype == 'json':
+            elif self.RETURNTYPE == 'json':
                 data = urldata.json()
             else:
-                success = False
-                data = ''
                 loglines.append( 'unable to convert returned object to acceptable type' )
-                loglines.append( urldata )
+                return False, loglines, ''
         else:
-            success = False
-            data = ''
+            return False, loglines, ''
         loglines.append( '-----URL OBJECT RETURNED-----' )
         loglines.append( data )
-        return success, loglines, data
+        return urldata.status_code, loglines, data
 
 
     def _unpack_args( self, kwargs ):
         try:
+            auth = kwargs['auth']
+        except KeyError:
+            auth = ()
+        try:
             params = kwargs['params']
-        except IndexError:
+        except KeyError:
             params = {}
         try:
-            data = kwargs['data']
+            thedata = kwargs['data']
         except KeyError:
-            if self.returntype == 'json':
-                data = []
+            if self.RETURNTYPE == 'json':
+                thedata = []
             else:
                 data = ''
-        return params, data
+        return auth, params, thedata
